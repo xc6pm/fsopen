11cd254931805bab85477e8aa5afbbcb2edb51cc
hossein <h.ebr141@outlook.com>
2024-07-06 00:13:19 +0330

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react"
 import "./app.css"
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const pointsEarned = good + (bad * -1)
+  const average = pointsEarned / all
+  const positiveRatio = good / all
+
+  return (
+    <>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positiveRatio}</p>
+    </>
+  )
+}
+
 function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   return (
     <>
       <h1>give feedback</h1>
@@ -14,9 +32,7 @@ function App() {
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
