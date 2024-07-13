9c82daa408221969262ba1bad3cd822d1dd8f38c
hossein <h.ebr141@outlook.com>
2024-07-13 15:13:21 +0330

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
+import axios from "axios"
+import { useEffect, useState } from 'react'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '09443535455', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  })
 
   const personsToShow = query ?
     persons.filter(p => p.name.toLowerCase().includes(query.toLowerCase())) :
