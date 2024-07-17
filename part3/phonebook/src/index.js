4be698136cebbdf702d582d0c263d2ccf2d9effa
hossein <h.ebr141@outlook.com>
2024-07-17 12:57:29 +0330

    3.3

---
+++ part3/phonebook/src/index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+
+const app = express()
+
+const data = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get("/api/persons", (request, response) => {
+  response.json(data)
+})
+
+app.get("/info", (request, response) => {
+  response.send(`Phonebook has data info for ${data.length} people <br/> ${new Date()}`)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  const person = data.find(p => p.id == request.params.id)
+
+  if (person)
+    response.json(person)
+  else
+    response.sendStatus(404)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => "Server running on port " + PORT)
