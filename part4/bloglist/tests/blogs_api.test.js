4454a0dfb4e3aed66330bd1c4ee52bbaae130f8f
hossein <h.ebr141@outlook.com>
2024-08-04 20:07:59 +0330

    4.11

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,5 +1,5 @@
 const supertest = require("supertest")
-const { test, after, beforeEach } = require("node:test")
+const { test, after, beforeEach, describe } = require("node:test")
 const mongoose = require("mongoose")
 const app = require("../app")
 const Blog = require("../models/blog")
@@ -53,36 +53,56 @@ beforeEach(async () => {
   }
 })
 
-test("initial notes are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/)
-    .expect((c) => c.body.length === initialBlogs.length)
+describe("get blogs", () => {
+  test("initial notes are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/)
+      .expect((c) => c.body.length === initialBlogs.length)
+  })
+  
+  test("_id is renamed to id", async () => {
+    const response = await api.get("/api/blogs")
+    assert.ok("id" in response.body[0])
+    assert.ok(!("_id" in response.body[0]))
+  })
 })
 
-test("_id is renamed to id", async () => {
-  const response = await api.get("/api/blogs")
-  assert.ok("id" in response.body[0])
-  assert.ok(!("_id" in response.body[0]))
-})
 
-test("blog gets added", async () => {
+describe("post blogs", () => {
   const newBlog = {
     title: "There is no thread",
     author: "Stephen Cleary",
     url: "https://blog.stephencleary.com/2013/11/there-is-no-thread.html",
     likes: 261,
   }
-  await api.post("/api/blogs").send(newBlog).expect(201)
 
-  const updatedBlogs = await api.get("/api/blogs")
+  test("blog gets added", async () => {
+    await api.post("/api/blogs").send(newBlog).expect(201)
+  
+    const updatedBlogs = await api.get("/api/blogs")
+  
+    assert.strictEqual(updatedBlogs.body.length, initialBlogs.length + 1)
+  
+    delete updatedBlogs.body[updatedBlogs.body.length - 1].id
+    assert.deepStrictEqual(updatedBlogs.body[updatedBlogs.body.length - 1], newBlog)
+  })
+  
+  test("likes defaults to zero", async () => {
+    delete newBlog.likes
 
-  assert.strictEqual(updatedBlogs.body.length, initialBlogs.length + 1)
+    await api.post("/api/blogs").send(newBlog).expect(201)
 
-  delete updatedBlogs.body[updatedBlogs.body.length - 1].id
-  assert.deepStrictEqual(updatedBlogs.body[updatedBlogs.body.length - 1], newBlog)
+    const updatedBlogs = await api.get("/api/blogs")
+
+    assert.strictEqual(updatedBlogs.body.length, initialBlogs.length + 1)
+
+    assert.ok("likes" in updatedBlogs.body[updatedBlogs.body.length - 1])
+    assert.equal(updatedBlogs.body[updatedBlogs.body.length - 1].likes, 0)
+  })
 })
+
 
 after(async () => {
   await mongoose.connection.close()
