9d0e7266811050661ca2ddb86ef82fcf986f623e
hossein <h.ebr141@outlook.com>
2024-08-06 23:31:24 +0330

    4.22

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -20,9 +20,8 @@ app.use(express.json())
 
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
-app.use(middleware.userIdExtractor)
 
-app.use("/api/blogs", blogsRouter)
+app.use("/api/blogs", middleware.userIdExtractor, blogsRouter)
 app.use("/api/users", usersRouter)
 app.use("/api/login", loginRouter)
 
