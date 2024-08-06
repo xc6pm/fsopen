e4f954298b3cb1022711e4d03705c16a5dbc0da2
hossein <h.ebr141@outlook.com>
2024-08-06 20:03:41 +0330

    4.16

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -14,6 +14,9 @@ usersRouter.post("/", async (request, response, next) => {
   if (!username || !name | !password)
     return response.status(400).json("username, name, and password must be provided")
 
+  if (password.length < 8)
+    return response.status(400).json({error: "the password must be at least 8 characters"})
+
   const passwordHash = await bcrypt.hash(password, 10)
   try {
     const result = await User.create({ name, username, passwordHash })
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, minLength: 3 },
   name: String,
   passwordHash: { type: String, required: true, minLength: 8 },
 },{
