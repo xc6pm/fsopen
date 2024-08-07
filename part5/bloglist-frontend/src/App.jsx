3ae28d1f5d396cdad1b6aabc223b05f0f6c72938
hossein <h.ebr141@outlook.com>
2024-08-07 13:25:26 +0330

    5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -6,20 +6,40 @@ import LoginForm from "./components/LoginForm";
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
-  
+
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
+  useEffect(() => {
+    const loggedInUserJson = window.localStorage.getItem("blogsLoggedInUser");
+
+    if (loggedInUserJson) {
+      setUser(JSON.parse(loggedInUserJson));
+    }
+  }, []);
+
+  const handleUserChange = (newUser) => {
+    window.localStorage.setItem("blogsLoggedInUser", JSON.stringify(newUser));
+    setUser(newUser);
+  };
+
+  const logout = () => {
+    window.localStorage.removeItem("blogsLoggedInUser");
+    setUser(null);
+  };
+
   if (!user) {
-    return (<LoginForm onUserChanged={newUser => setUser(newUser)}/>);
+    return <LoginForm onUserChanged={handleUserChange} />;
   }
 
   return (
     <div>
       <h2>blogs</h2>
 
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in <button onClick={logout}>log out</button>
+      </p>
 
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
