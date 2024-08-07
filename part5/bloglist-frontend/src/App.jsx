d3f5e023f80bb1bea0ae9a36f38aa6cbb64cdc1b
hossein <h.ebr141@outlook.com>
2024-08-07 17:58:58 +0330

    5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -64,7 +64,7 @@ const App = () => {
         </p>
       )}
 
-      {blogs.map((blog) => (
+      {blogs.sort((a, b) => b.likes - a.likes).map((blog) => (
         <Blog key={blog.id} blog={blog} blogsUpdated={refreshBlogs} showMessage={showMessage}/>
       ))}
 
