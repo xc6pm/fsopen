17250a32138902ef35c434f42ee9940f3940642b
hossein <h.ebr141@outlook.com>
2024-08-07 17:44:27 +0330

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const toggleDetailsVisible = () => {
+    setDetailsVisible(!detailsVisible);
+  };
+
+  return (
+    <div style={{border: "2px solid black", margin: "5px", padding: "3px"}}>
+      {blog.title} {blog.author}{" "}
+      <button onClick={toggleDetailsVisible}>
+        {detailsVisible ? "hide" : "more"}
+      </button>
+      <div style={{ display: detailsVisible ? "" : "none" }}>
+        {blog.url}
+        <br />
+        <span>
+          {blog.likes} <button>like</button>
+        </span>
+        <br />
+        {blog.creator.name}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
