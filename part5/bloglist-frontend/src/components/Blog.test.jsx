2ad10056879877c0501ebb321a5371482dd0f09e
hossein <h.ebr141@outlook.com>
2024-08-10 15:12:15 +0330

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,20 +1,20 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Blog from "./Blog"
 import { expect } from "vitest"
 
-test("Renders only blog name and author", () => {
-    const blog = {
-        title: "Arthur Mikoyan running through the hills",
-        author: "John",
-        likes: 1,
-        url: "arthormik.com",
-        creator: {
-            id: "33",
-            username: "aun",
-            name: "un"
-        }
+const blog = {
+    title: "Arthur Mikoyan running through the hills",
+    author: "John",
+    likes: 1,
+    url: "arthormik.com",
+    creator: {
+        id: "33",
+        username: "aun",
+        name: "un"
     }
+}
 
+test("Renders only blog name and author", () => {
     render(<Blog blog={blog}/>)
 
     const titleEl = screen.getByText(blog.title, {exact: false})
@@ -29,3 +29,21 @@ test("Renders only blog name and author", () => {
     const urlEl = screen.getByText(blog.url, {exact: false})
     expect(urlEl).not.toBeVisible()
 })
+
+test("pressing more button displays likes and url", () => {
+
+    render(<Blog blog={blog}/>)
+
+    const moreButton = screen.getByText("more")
+
+    fireEvent.click(moreButton)
+
+    const likesEl = screen.getByTestId("likes")
+    expect(likesEl).toBeVisible()
+
+    const urlEl = screen.getByText(blog.url, {exact: false})
+    expect(urlEl).toBeVisible()
+})
+
+
+test("")
